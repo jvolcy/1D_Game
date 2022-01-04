90c37a72a6587893c0716acfe7e41a91c9b1082d
jvolcy <jerry.volcy@gmail.com>
2022-01-03 20:27:34 -0500

    Canons added

---
+++ Assets/GOs/Canon/CanonBall.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonBall : MonoBehaviour
+{
+    public GameObject OrbPrefab;
+    public Sprite[] OrbSprites;
+    public float MinFiringForce = 500f;
+    public float MaxFiringForce = 1100f;
+    public float CanonBallLife = 10f;
+    public float CanonAngle = 145f;
+    public float FiringPeriod = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 100 == 0)
+        {
+            GameObject orb;
+
+            orb = Instantiate(OrbPrefab, transform.position, new Quaternion());
+            Orb orbScript = orb.GetComponent<Orb>();
+
+            orbScript.Force = Random.Range(MinFiringForce, MaxFiringForce);
+            orbScript.Angle = CanonAngle;
+            orbScript.OrbLife = CanonBallLife;
+
+            SpriteRenderer spriteRenderer = orb.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = OrbSprites[Random.Range(0, OrbSprites.Length)];
+        }
+        
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,10 +6,16 @@ public class player : MonoBehaviour
 {
     public Camera mainCamera;
     public float Speed = 2f;
+    float horizontalSpeed = 0f;     // -Speed, +Speed or 0
     public float jumpForce = 300f;
+    public float minX = -20f;
+    public float maxX = 30f;
+
+    public Sprite orb;
 
     Animator animator;
     Rigidbody2D rigidbody2d;
+    bool bOnGround = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,40 +26,76 @@ public class player : MonoBehaviour
         animator.SetBool("bWalking", false);
 
         rigidbody2d = GetComponent<Rigidbody2D>();
+        bOnGround = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (!bOnGround)
         {
-            if (transform.position.x < 20f) transform.Translate(Speed * Time.deltaTime * transform.right);
+            /* if we are in the air, continue the player's forward momentum.
+             That is, continue to apply the last horizontalSpeed until the
+            play is once more on the ground. */
+            updatePlayerPosition();
+            updateCameraPosition();
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalSpeed = Speed;
+            updatePlayerPosition();
             updateCameraPosition();
             animator.SetBool("bDirRight", true);
             animator.SetBool("bWalking", true);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (transform.position.x > -20f) transform.Translate(-Speed * Time.deltaTime * transform.right);
+            horizontalSpeed = -Speed;
+            updatePlayerPosition();
             updateCameraPosition();
             animator.SetBool("bDirRight", false);
             animator.SetBool("bWalking", true);
         }
         else
         {
+            horizontalSpeed = 0f;
             animator.SetBool("bWalking", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))        //jump
+        if (Input.GetKeyDown(KeyCode.UpArrow) && bOnGround)        //jump
         {
             rigidbody2d.AddForce(jumpForce * transform.up);
         }
     }
 
 
+    void updatePlayerPosition()
+    {
+        transform.Translate(horizontalSpeed * Time.deltaTime * transform.right);
+        if (transform.position.x > maxX) transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        if (transform.position.x < minX) transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+
+    }
+
     void updateCameraPosition()
     {
-        mainCamera.transform.position = new Vector3(Mathf.Clamp(transform.position.x, -12.5f, 12.5f), mainCamera.transform.position.y, mainCamera.transform.position.z);
+        mainCamera.transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX+7.5f, maxX-7.5f), mainCamera.transform.position.y, mainCamera.transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        //Debug.Log("CollisionEnter: " + otherObject.name);
+
+        bOnGround = true;
+        //if (otherObject.name == "TM Ground") bOnGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        //Debug.Log("CollisionExit: " + otherObject.name);
+
+        if (otherObject.name == "TM Ground") bOnGround = false;
     }
 }
